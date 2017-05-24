a31cb3968cb4df1663f5b671655c9a23b94f6f3e
Kevin Lui <klui@uw.edu>
2017-05-24 18:57:19 +0000

    inital version of ulam_set

---
+++ ulam_set.py
@@ -0,0 +1,50 @@
+from sys import argv
+
+
+def infinity_norm(v):
+    return max(abs(v[0]), abs(v[1]))
+
+
+def two_norm_squared(v):
+    return v[0]**2 + v[1]**2
+
+
+def vector_sum(v, w):
+    return (v[0]+w[0], v[1]+w[1])
+
+
+def compute_ulam_set(n, init_vectors=[(1, 0), (0, 1)], norm=infinity_norm):
+    # ulam_set is the set of all ulam elements found so far
+    ulam_set = set(init_vectors)
+
+    # new_ulam is the set of all ulam elements found in the latest iteration
+    new_ulam = set(init_vectors)
+
+    # pairwise sums is the set of all pairwise sums of all ulam elements found
+    # so far that are not already in ulam_set
+    pairwise_sums = set([])
+
+    for _ in range(n):
+        # update pairwise sums by computing pairwise sums between new_ulam
+        # elements and ulam_set elements and substracting ulam_set
+        new_sums = [vector_sum(x, y) for x in ulam_set for y in new_ulam
+                    if x != y]
+        pairwise_sums = pairwise_sums.union(new_sums)
+        pairwise_sums = pairwise_sums.difference(ulam_set)
+
+        # update new_ulam to be the set of pairwise sums of smallest norm
+        smallest_norm = min([norm(x) for x in pairwise_sums])
+        new_ulam = set([x for x in pairwise_sums if norm(x) == smallest_norm])
+
+        # update ulam_set to include new_ulam
+        ulam_set = ulam_set.union(new_ulam)
+
+    return ulam_set
+
+try:
+    m = int(argv[1])
+except:
+    m = 10
+ulam = compute_ulam_set(m)
+# print(ulam)
+print(len(ulam))
