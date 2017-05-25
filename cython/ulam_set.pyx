b476ca07da2b19b9be8376ad59d96e1ba7c7f66a
kevin lui <kevinywlui@gmail.com>
2017-05-24 21:46:57 -0700

    using maxie's stuff

--- cython/ulam_set.pyx
+++ cython/ulam_set.pyx
@@ -1,7 +1,8 @@
-cdef int infinity_norm(v):
+cdef infinity_norm(v):
     return max(abs(v[0]), abs(v[1]))
 
-cdef int two_norm_squared(v):
+
+cdef two_norm_squared(v):
     return v[0]**2 + v[1]**2
 
 
@@ -9,9 +10,10 @@ cdef vector_sum(v, w):
     return (v[0]+w[0], v[1]+w[1])
 
 
-cpdef compute_ulam_set(int n, init_vectors=[(1, 0), (0, 1)], norm=infinity_norm):
+cdef compute_ulam_set(n, init_vectors=[(1, 0), (0, 1)], norm=infinity_norm):
     # ulam_set is the set of all ulam elements found so far
     ulam_set = set(init_vectors)
+    old_ulam_set = ulam_set.copy()
 
     # new_ulam is the set of all ulam elements found in the latest iteration
     new_ulam = set(init_vectors)
@@ -19,21 +21,43 @@ cpdef compute_ulam_set(int n, init_vectors=[(1, 0), (0, 1)], norm=infinity_norm)
     # pairwise sums is the set of all pairwise sums of all ulam elements found
     # so far that are not already in ulam_set
     pairwise_sums = set([])
+    unique_counts = dict()
 
-    cdef i
-    for i in range(n):
+    for nidx in range(n):
         # update pairwise sums by computing pairwise sums between new_ulam
         # elements and ulam_set elements and substracting ulam_set
-        new_sums = [vector_sum(x, y) for x in ulam_set for y in new_ulam
+        new_sums = [vector_sum(x, y) for x in old_ulam_set for y in new_ulam
                     if x != y]
-        pairwise_sums = pairwise_sums.union(new_sums)
+        new_ulam = list(new_ulam)
+        for (xidx, x) in enumerate(new_ulam):
+             for yidx in range(xidx + 1, len(new_ulam)):
+                  y = new_ulam[yidx]
+                  new_sums += [vector_sum(x, y)]
+             ##
+        ##
+        for nsum in new_sums:
+             if nsum in unique_counts and nidx > 0:
+                  unique_counts[nsum] += 1
+             else:
+                  unique_counts[nsum] = 1
+        ##
+        pairwise_sums = pairwise_sums.union(set(new_sums))
         pairwise_sums = pairwise_sums.difference(ulam_set)
+
+        # remove elements that are not uniquely represented:
+        pairwise_sums_temp = pairwise_sums.copy()
+        for v in pairwise_sums:
+             if unique_counts[v] > 1:
+                  pairwise_sums_temp.remove(v)
+        ##
+        pairwise_sums = pairwise_sums_temp
 
         # update new_ulam to be the set of pairwise sums of smallest norm
         smallest_norm = min([norm(x) for x in pairwise_sums])
         new_ulam = set([x for x in pairwise_sums if norm(x) == smallest_norm])
 
         # update ulam_set to include new_ulam
+        old_ulam_set = ulam_set.copy()
         ulam_set = ulam_set.union(new_ulam)
 
     return ulam_set
